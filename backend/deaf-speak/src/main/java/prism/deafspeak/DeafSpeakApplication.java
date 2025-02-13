50ed25cbbf5727500014f3332504977d1a42df38
modelable <62459471+modelable@users.noreply.github.com>
2025-02-13 17:24:25 +0900

    create project

---
+++ backend/deaf-speak/src/main/java/prism/deafspeak/DeafSpeakApplication.java
@@ -0,0 +1,13 @@
+package prism.deafspeak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeafSpeakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeafSpeakApplication.class, args);
+    }
+
+}
---
+++ backend/deaf-speak/src/test/java/prism/deafspeak/DeafSpeakApplicationTests.java
@@ -0,0 +1,13 @@
+package prism.deafspeak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeafSpeakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
