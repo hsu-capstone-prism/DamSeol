86c50b34d63b08a189963cfd94e4ad594efc9cc2
modelable <62459471+modelable@users.noreply.github.com>
2025-02-20 11:39:31 +0900

    change project name

---
+++ backend/DamSeol/src/main/java/prism/deafspeak/DamSeolApplication.java
@@ -0,0 +1,13 @@
+package prism.deafspeak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamSeolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DamSeolApplication.class, args);
+    }
+
+}
