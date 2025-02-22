b9c52f88b88206c8163c4000f6aded1febbcfe97
mllnxeo <minseojo1@naver.com>
2025-02-23 02:28:56 +0900

    WordPage 추가 및 css 수정

---
+++ frontend/src/pages/MainPage/ClassPage/SentencePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../Layout";
+
+const SentencePage = () => {
+  return (
+    <Layout>
+      <h1>문장 페이지입니다.</h1>
+      <p>여기에 문장 학습 관련 콘텐츠를 추가하세요.</p>
+    </Layout>
+  );
+};
+
+export default SentencePage;
