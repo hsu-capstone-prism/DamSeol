5010d37d91686d83e99327274a997a993f78f598
mllnxeo <minseojo1@naver.com>
2025-03-04 14:51:15 +0900

    sentencepage 파일 구조 변경

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,20 +15,9 @@ import ConsonantPage from "./pages/MainPage/ClassPage/WordPage/PhonPage/Consonan
 import StudyPage from "./pages/MainPage/ClassPage/WordPage/PhonPage/ConsonantPage/StudyPage";
 
 //SentencePage
-import RestaurantPage from "./pages/MainPage/ClassPage/SentencePage/SpecialPage/RestaurantPage/RestaurantPage";
-import ChurchPage from "./pages/MainPage/ClassPage/SentencePage/SpecialPage/ChurchPage/ChurchPage";
-import HospitalPage from "./pages/MainPage/ClassPage/SentencePage/SpecialPage/HospitalPage/HospitalPage";
-import AirportPage from "./pages/MainPage/ClassPage/SentencePage/SpecialPage/AirportPage/AirportPage";
-import ShoppingPage from "./pages/MainPage/ClassPage/SentencePage/SpecialPage/ShoppingPage/ShoppingPage";
-import TripPage from "./pages/MainPage/ClassPage/SentencePage/SpecialPage/TripPage/TripPage";
-import TransportPage from "./pages/MainPage/ClassPage/SentencePage/SpecialPage/TransportPage/TransportPage";
-
-import ITPage from "./pages/MainPage/ClassPage/SentencePage/BusinessPage/ITPage/ITPage";
-import MarketingPage from "./pages/MainPage/ClassPage/SentencePage/BusinessPage/MarketingPage/MarketingPage";
-import SalesPage from "./pages/MainPage/ClassPage/SentencePage/BusinessPage/SalesPage/SalesPage";
-import HRPage from "./pages/MainPage/ClassPage/SentencePage/BusinessPage/HRPage/HRPage";
-import DesignPage from "./pages/MainPage/ClassPage/SentencePage/BusinessPage/DesignPage/DesignPage";
-import ResearchPage from "./pages/MainPage/ClassPage/SentencePage/BusinessPage/ResearchPage/ResearchPage";
+import BusinessPage from "./pages/MainPage/ClassPage/SentencePage/BusinessPage/BusinessPage";
+import SpecialPage from "./pages/MainPage/ClassPage/SentencePage/SpecialPage/SpecialPage";
+import sentenceData from "./data/sentenceData";
 
 function AppContent() {
   const location = useLocation();
@@ -39,7 +28,6 @@ function AppContent() {
     { path: "/login", element: <LoginPage /> },
     { path: "/signup", element: <SignupPage /> },
     { path: "/main", element: <MainPage /> },
-    { path: "/word", element: <WordPage /> },
     { path: "/report", element: <ReportPage /> },
 
     //word
@@ -50,21 +38,6 @@ function AppContent() {
 
     //sentence
     { path: "/sentence", element: <SentencePage /> },
-    //special
-    { path: "/sentence/church", element: <ChurchPage /> },
-    { path: "/sentence/restaurant", element: <RestaurantPage /> },
-    { path: "/sentence/hospital", element: <HospitalPage /> },
-    { path: "/sentence/airport", element: <AirportPage /> },
-    { path: "/sentence/shopping", element: <ShoppingPage /> },
-    { path: "/sentence/trip", element: <TripPage /> },
-    { path: "/sentence/transport", element: <TransportPage /> },
-    //business
-    { path: "/sentence/IT", element: <ITPage /> },
-    { path: "/sentence/marketing", element: <MarketingPage /> },
-    { path: "/sentence/sales", element: <SalesPage /> },
-    { path: "/sentence/HR", element: <HRPage /> },
-    { path: "/sentence/design", element: <DesignPage /> },
-    { path: "/sentence/research", element: <ResearchPage /> },
 
     //grammer
     { path: "/grammer", element: <GrammerPage /> },
@@ -77,6 +50,15 @@ function AppContent() {
         {routes.map(({ path, element }, key) => (
           <Route path={path} element={element} key={key} />
         ))}
+        {/* sentence */}
+        {sentenceData.map(({ id }) => (
+          <Route
+            key={id}
+            path={`/sentence/:category`}
+            element={<BusinessPage />}
+          />
+        ))}
+        <Route path={`/sentence/:category`} element={<SpecialPage />} />
       </Routes>
     </div>
   );
