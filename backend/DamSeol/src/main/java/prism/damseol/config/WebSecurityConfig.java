62ee2aeef05623ba7755b978ef9bd90f66f41fb2
modelable <62459471+modelable@users.noreply.github.com>
2025-03-04 12:47:28 +0900

    cors settings

--- backend/DamSeol/src/main/java/prism/damseol/config/WebSecurityConfig.java
+++ backend/DamSeol/src/main/java/prism/damseol/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package prism.damseol.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +12,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import prism.damseol.jwt.JWTFilter;
 import prism.damseol.jwt.JWTUtil;
 import prism.damseol.jwt.LoginFilter;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -38,6 +43,27 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //cors 설정
+        http
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                })));
 
         //csrf disable
         http
--- backend/DamSeol/src/main/java/prism/damseol/controller/MainController.java
+++ backend/DamSeol/src/main/java/prism/damseol/controller/MainController.java
@@ -1,8 +1,15 @@
 package prism.damseol.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import prism.damseol.domain.Role;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @ResponseBody
@@ -10,6 +17,16 @@ public class MainController {
 
     @GetMapping("/")
     public String mainP() {
-        return "Main Controller";
+        //세션 사용자 이름 확인
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        //세션 사용자 role 확인
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        List<String> roles = auth.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        return "Main Controller : " + name;
     }
 }
