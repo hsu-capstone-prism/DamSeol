dbdb0b0d3ff297b3bdfb59d628452af8cceeddf9
mllnxeo <minseojo1@naver.com>
2025-03-24 15:47:10 +0900

    css 수정

--- frontend/src/pages/MainPage/ClassPage/GrammerPage.jsx
+++ frontend/src/pages/MainPage/ClassPage/GrammerPage.jsx
@@ -8,15 +8,13 @@ const GrammerPage = () => {
       <div className="grammer-page">
         <h1 className="section-title">문법 연습</h1>
         <section className="grammer-learning-section">
-          <div className="box-container">
-            <div className="box">기본 문장 구조</div>
-            <div className="box">문장 내 모음 발음</div>
-            <div className="box">문장 내 자음 발음</div>
-            <div className="box">문장 내 음운 변동</div>
-            <div className="box">사잇소리 현상</div>
-            <div className="box">문장 내 불규칙 활용</div>
-            <div className="box">..</div>
-            <div className="box">..</div>
+          <div className="grammer-box-container">
+            <div className="grammer-box">기본 문장 구조</div>
+            <div className="grammer-box">문장 내 모음 발음</div>
+            <div className="grammer-box">문장 내 자음 발음</div>
+            <div className="grammer-box">문장 내 음운 변동</div>
+            <div className="grammer-box">사잇소리 현상</div>
+            <div className="grammer-box">문장 내 불규칙 활용</div>
           </div>
         </section>
       </div>
