da52775089014592ce0146504c1e339ad772212f
sseung6 <osmksh1004@naver.com>
2025-04-30 12:49:58 +0900

    mainpage 수정

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../../styles/MainPage.css";
-import wordImg from "../../assets/images/word.png";
-import sentenceImg from "../../assets/images/sentence.png";
-import grammerImg from "../../assets/images/grammer.png";
 
 const MainPage = () => {
   const navigate = useNavigate();
@@ -14,53 +11,37 @@ const MainPage = () => {
 
   return (
     <main className="main">
-      <section className="class-section">
-        <h2>My Class</h2>
-        <div className="class-container">
-          <div className="class-box" onClick={() => handleClick("/word")}>
-            <img
-              src={wordImg}
-              alt="단어 학습"
-              style={{ width: "100%", height: "120%", objectFit: "contain" }}
-            />
+      <section className="grid-section">
+        <div className="grid-container">
+          <div
+            className="grid-box box-large"
+            onClick={() => handleClick("/word")}
+          >
+            단어 학습
           </div>
-          <div className="class-box" onClick={() => handleClick("/sentence")}>
-            <img
-              src={sentenceImg}
-              alt="문장 학습"
-              style={{ width: "100%", height: "120%", objectFit: "contain" }}
-            />
+          <div
+            className="grid-box box-normal"
+            onClick={() => handleClick("/game")}
+          >
+            게임
           </div>
-          <div className="class-box" onClick={() => handleClick("/grammer")}>
-            <img
-              src={grammerImg}
-              alt="문법 연습"
-              style={{ width: "100%", height: "120%", objectFit: "contain" }}
-            />
+          <div
+            className="grid-box box-normal"
+            onClick={() => handleClick("/report")}
+          >
+            리포트
           </div>
-        </div>
-      </section>
-      <section className="report-section">
-        <h2>Report</h2>
-        <div
-          className="report-box"
-          onClick={() => handleClick("/report")}
-        ></div>
-      </section>
-      <section className="score-section">
-        <h2>Score</h2>
-        <div className="score-table">
-          <div className="score-row">
-            <span>A</span>
-            <div className="score-line"></div>
+          <div
+            className="grid-box box-normal"
+            onClick={() => handleClick("/sentence")}
+          >
+            문장 학습
           </div>
-          <div className="score-row">
-            <span>B</span>
-            <div className="score-line"></div>
-          </div>
-          <div className="score-row">
-            <span>C</span>
-            <div className="score-line"></div>
+          <div
+            className="grid-box box-normal"
+            onClick={() => handleClick("/grammer")}
+          >
+            문법 연습
           </div>
         </div>
       </section>
