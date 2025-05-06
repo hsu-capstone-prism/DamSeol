8cde5a590112dced39ee8682bed4944548ddf9fb
HeewookYoon <66108138+yhw032@users.noreply.github.com>
2025-05-06 21:26:55 +0900

    css refactoring

--- frontend/src/pages/MainPage/GamePage/GamePage.jsx
+++ frontend/src/pages/MainPage/GamePage/GamePage.jsx
@@ -6,6 +6,13 @@ const GameScene = () => {
     alert(`선택한 문장: ${choice}`);
   };
 
+  const gameData = {
+    "context": "허리 통증으로 진단을 받은 당신에게 의사가 심각한 표정으로 말합니다.",
+    "video": "/videos/spine.mp4",
+    "choices": ["A. 이게 환자분의 척추입니다.", "B. 척추가 많이 휘어있습니다.", "C. 척추를 한번 살펴보았습니다."],
+    "answer": "A"
+  }
+
   return (
     <div className="game-container">
   <section className="game-section">
@@ -13,19 +20,20 @@ const GameScene = () => {
     <div className="game-box-wrapper">
       <div className="media-section">
         <video width="100%" controls>
-          <source src="/videos/spine.mp4" type="video/mp4" />
+          <source src={gameData.video} type="video/mp4" />
           브라우저가 비디오를 지원하지 않습니다.
         </video>
       </div>
       <div className="text-section">
         <p className="description">
-          허리 통증으로 진단을 받은 당신에게<br />
-          의사가 심각한 표정으로 말합니다.
+          {gameData.context}
         </p>
         <div className="choices">
-          <button onClick={() => handleAnswer("A")}>A. 이게 환자분의 척추입니다.</button>
-          <button onClick={() => handleAnswer("B")}>B. 척추가 많이 휘어있습니다.</button>
-          <button onClick={() => handleAnswer("C")}>C. 척추를 한번 살펴보았습니다.</button>
+          {gameData.choices.map((choice, index) => (
+            <button key={index} onClick={() => handleAnswer(choice)}>
+              {choice}
+            </button>
+          ))}
         </div>
       </div>
     </div>
