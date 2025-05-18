43db393584d82a0433a79dfa915f2622083caa26
HeewookYoon <66108138+yhw032@users.noreply.github.com>
2025-05-18 21:51:35 +0900

    prompt update

--- python/services/evaluate_test.py
+++ python/services/evaluate_test.py
@@ -12,9 +12,11 @@ from services.extract_feature import extract_speech_rate, extract_pitch_info, ex
 #get_audio_pitch_eval("E:\KsponSpeech\original\KsponSpeech_01\KsponSpeech_0001\KsponSpeech_000001.pcm", "아 모 몬소리야 그건 또")
 #get_audio_rhythm_eval("E:\KsponSpeech\original\KsponSpeech_01\KsponSpeech_0001\KsponSpeech_000001.pcm", "아 모 몬소리야 그건 또")
 
-PATH = "C:\\Users\\admin\\Documents\my-project\\project-deaf-learning\\python\\test.wav"
+#PATH = "C:\\Users\\admin\\Documents\my-project\\project-deaf-learning\\python\\test.wav"
+PATH = "C:\\Users\\admin\\Documents\\my-project\\project-deaf-learning\\backend\\DamSeol\\uploads\\audio\\1747302746421_audio.wav"
 
 #print(get_audio_rhythm_eval(PATH, "안녕하세요? 저는 몽골에서 온 자가라고 해요."))
 #print(get_audio_pitch_eval(PATH, "안녕하세요? 저는 몽골에서 온 자가라고 해요."))
 text = get_pronun(PATH)
-print(evaluate_pronunciation("안녕하세요? 저는 몽골에서 온 자가라고 해요.", text))
+if text:
+  print(evaluate_pronunciation("현재 제품이 고객의 요구사항을 충족할 수 있을까요?", text))
